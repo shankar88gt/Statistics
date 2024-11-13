08d50a6d4d5be3b717f82c27a7992f05015d8e62
shankar88gt <shankar.88.gt@gmail.com>
2024-11-12 21:43:01 -0600

    Statis Inferential Statistics

--- Statistics_Datatab.py
+++ Statistics_Datatab.py
@@ -32,7 +32,7 @@ Inferential Statistics - draw conclusions based on sample data about the populat
                 we test the null hypothesis; and not alternate
                     e.g all swans are white. instead see if there an evidence to find atleast one black swan which proves alternate is incorrect
                                 
-        eg. Res Hyp - The drug has an effect on blood pressure
+        eg. Research Hyp - The drug has an effect on blood pressure
             Null - the no effect  assumption
 
             A random sample is drawn and say if the drug has large effect. 
@@ -117,7 +117,7 @@ Inferential Statistics - draw conclusions based on sample data about the populat
 
             ANOVA ( One WAY)
                 Types 
-                    Single factorial without measurement repetition - we will jut talk about this one
+                    Single factorial without measurement repetition - we will just talk about this one
                     Single factorial with measurement repetition
                     Two factorial without measurement repetition
                     TWO factorial with measurement repetition
@@ -132,11 +132,11 @@ Inferential Statistics - draw conclusions based on sample data about the populat
                             Dependent variable - age ( Metric )
                             population - inferential statistics based on sample
                         Hypothesis
-                            Null -  There are no differences between the population between the means of individual groups
+                            Null -  There are no differences between the means of individual groups
                             Alternate :  atleast 2 groups differ from each other in the population
-            ANOVA ( TWo WAY )
-                Statistical method used to test the effect of 2 categorical variables on a continuous variables
-                e.g.  drug type - A , B  & Gender - male & Female ( Factors - Nominal variable )
+            ANOVA ( TWO WAY )
+                Statistical method used to test the effect of 2 categorical variables on a continuous variable
+                e.g.  drug type - A , B  & Gender - Male & Female ( Factors - Nominal variable )
                       blood pressure - metric - continuous variable
                     
                 What kind of statements can we make with a ANOVA?
@@ -191,10 +191,12 @@ Inferential Statistics - draw conclusions based on sample data about the populat
                     Normality  - not normal then non parametric 
                     Sphericity - the variances of the diff between all combinations of factor levels ( time points ) shd be the same  - Mauchly's test of sphericity
                        if P value > 0.05 then the variances are equal 
-                
-                if P < 0.05 then H1. do Bonferroni post-hoc tests to identify which group has that difference
+                            if violation then adjustments such as Greenhouse Geisser / Huynh Feldt test can be used - sphericity correction
 
                 F distribution table and then P value 
+                    Our goal  is to calculate the F value and then calculate P value
+                
+                if P < 0.05 then H1. do Bonferroni post-hoc tests to identify which group has that difference
 
             Mixed Model ANOVA - 2 two way ANOVA with repeated measues
                  A mixed model ANOVA is a statistical method used to analyze data that involve both between subject factors and within subject factors
@@ -226,7 +228,7 @@ Inferential Statistics - draw conclusions based on sample data about the populat
                             ANOVA is somewhat robust to violations of normality in large sample size
                     Homogeneity of variances - levene test
                         Needs to be true for both between subjects and within subjects factors
-                    Homogeneity of covariances ( Sphericity ) - mauchly's test - when violated ( Greenhouse-geisser or huynn feldt )
+                    Homogeneity of covariances ( Sphericity ) - mauchly's test - when violated ( Greenhouse-geisser or huynn feldt ) - within subject 
                         this applies to within subjects factor - the variances of the diff between all combinations of diff groups are equal
                             e.g. factor 2 - measurements at diff times
                                     grp1 - grp2 , grp1 - grp3, grp2 - grp3
@@ -252,16 +254,210 @@ Inferential Statistics - draw conclusions based on sample data about the populat
                     Parametric Test - Raw data
                     Non Parametrics test - Ranks of the data
                 
-                Goal                                        Parmetric test                        Non Parametric test
-                One Sample                                  Simple t-test                       Wilcoxon test for 1 sample
+                Goal                                      Parmetric test                        Non Parametric test
+                One Sample                                Simple t-test                         Wilcoxon test for 1 sample
                 Two dependent samples                     paired sample t-test                  Wilcoxon Test  
-                Two independent samples                   independent T test                    Mann Whitney U test
+                Two independent samples                   independent T test                    Mann Whitney U test ( ranks )
                 more than 2 independent samples           One factor ANOVA                      Kruskal Wallis Test
                 more than 2 dependent samples             Repeated Measures ANOVA               Friedman Test
-                corelation between 2 variables            Pearson corelation                    Spearmans rak corelation
+                corelation between 2 variables            Pearson corelation                    Spearmans rank corelation ( does not use raw data but ranks )
 
             Test For Normal Distribution
+                Test ur data for normal distribution
+                Has Bell Curve?
+
+                2 Ways
+                    1) Analytical
+                            Kolmogorov Smirnov Test ( other than normal distribution can also be tested )
+                            Shapiro Wilk Test
+                            Anderson Darling Test ( other than normal distribution can also be tested )
+
+                            for above H0: the data is normally distributed
+                                P value < 0.05 then Normal distrbution is not assumed
+
+                        Disadvantages of Analytical
+                            P value depends on sample size
+                            Distribution deviates very slightly from normal distribution - again on the sample size - we may reject the null Hyp
+
+                            Graphical is always better
+
+                    2) Graphical
+                            Histogram - bell curve
+                            Quantile-Quantile Plot
+
+            Equality of Variance - Levene's Test
+                Levene's test test the hypothesis that the variances are equal in different groups
+
+                Hypothesis:
+                    H0:  the variances of the group are equal ( P > 0.05 )
+                    H1:  Atleast one of the group has a diff variance
+
+                levene's test is used as a requirement for other hypothesis test's e.g T-test
+
+                Steps
+                1) Cal Mean of each group
+                2) subtract the respective group mean from each person
+                3) sum of those value from each group - the larger the sum the grater the variance
+                4) calculate total mean again
+                5) squared deviations of the group mean from the overall Mean and sum them up (1)
+                6) squared deviation within the groups (2)
+
+                L = ((num of cases - num of groups ) / ( num of group - 1) ) * ( SUM ( cases per grp * (1) ) / sum ( (2) ) )
+
+                Test Statistic L is equal to the F statistic
+
+                deg of freedom Numerator -  ( No of groups - 1 ) = K-1
+                deg of freedom denemenator - ( no of cases - no of grps ) = N-k
+
+            Mann Whitney U test ( ranks ) - Two independent samples - Non Parameteric 
+                it is based on ranks on the data and not raw data
+                e.g. gender & reaction time  - rank based on reaction time for the whole groups 
+
+                H0: both ranks sums are equal
+                H1 : both ranks sums are not equal
+
+                Manual Calculation
+                Step 1
+                    T1 - female - sum of ranks
+                    T2 - male - sum of ranks
+                Step 2 - calculate U
+                    U1 = n1.n2 + ((n1*(n2+1) / 2) - T1)                
+                    U2 = n1.n2 + ((n1*(n2+1) / 2) - T2)   
+                Step 3 - U value = min (U1,U2)
+                Step 4 = expected value of U
+                        Mu = n1*n2/2
+                Step 5 = standard eror of U
+                        Mu = sqrt(n1*n2*n1+n2+1/12)
+                Step 6 - Z value = U - Mu / Sigma U
+                Step 7 - calculate P value using Z value
+
+                    For upto 25 cases exact values can be used
+                    for larger samples, the normal distribution of the U value can be used as an approx.
+
+            Wilcoxon Test - Two dependent samples - Non Parametric
+                The rank is based on the diff in the mean between paired samples
+                   i.e the smallest diff gets the highest rank
+
+                Assumption and Hypothesis
+                    only two dependent random samples with at least ordinally scaled charaterstics need to be available
+                    the variables do not have to satisfy a distribution curve
+
+                    Hyp:
+                    H0: In the population, the mean of the two dependent samples are the same
+                    H1 : In the population, the mean of the two dependent samples are not the same
+
+                e.g. reaction time in morning & eve and are they diff?
+                    
+                Steps
+                    e.g.  Case(7), morning , eve,  diff( eve - morning - usually normally distributed else WilTest), Rank , cal of Rank sum ( retain sign )
+                    T+ = sum of ositive ranks
+                    T- = suum of neg ranks
+
+                    Test Statistic W = min ( T+,T-)
+                    Exp value of W - Mw - 
+                    Std error = Sigw
+
+                    Z-value =  W - Mw / Sigw
+
+                    if more than 25 cases , normal distribution is assumed
+                    else the Critical T value is read from a table 
+
+            Kruskal Wallis Test - Non Parametric ANOVA
+                Non parametric counter part to one way ANOVA
+                is there a diff in the rank totals ?
+
+                How must the variables be scaled?
+                    Independent Variable - nominal valiable with more than 2 possible values
+                    Dependent variable - salary, weight etc - Ranked
+
+                Assumptions
+                    Only several independent random samples with at least ordinally scaled characterstics must be available
+                    The variables do not have to satisfy a distribution curve - Since Rank & Ranksum is used 
+
+                    Hypothesis
+                        H0: The Independent samples all have the same central tendency and therefore come from the same pop
+                        H1: Atleast one of the independent samples does not have the same central tendency as the other samples and therefore come from a diff population
+
+                    E.g - reaction time of 3 diff groups - A,B & C
+
+                    1) Rank all 3 groups
+                    2) Ranksum & Mean Ranksum by groups  - Ra & RAb ( Mean )
+                    3) Expected value of Rank sum - Er = n+1/2
+                    4) Degree of freedom = Groups - 1 = 2
+                    5) Rank variance = n**2 - 1 / 12 ( no of entries )
+                    6) Test Statistic H - Chi Square value
+                    7) Table of Chi-Squared Distribution
+
+            FriedMan Test - Non Parametric ANOVA with repeated measures
+                Tests whether there are statistically significant differences between 3 or more dependent samples
+                Dependent sample - time dependent - measured at multiple time points
+                is there a diff in the rank totals?
+
+                H0:  No significant differences between the groups
+                H1:  there is a significant diff between the dependent groups
+
+                Calculation
+                e.g - you might be interested to know if therapy after a slipped ddisc has any effect on pain percetion
+                      measurement - before, middle & post therapy
+                      7 entries
+
+                1) Rank across ( per row ) - per sample ( largest value gets 1 )
+                2) Rank sum by measurements
+                3) Expected value - N(k+1)/2 
+                4) Chi**2 calculation
+                5) degree of freedom - k-1 
+                6) Table of Chi Squared distribution
+                7) if Chi**2 value is greater than Critical value then reject the null hypothesis
+
+            Chi**2 Test 
+                Test that is used when u want to determine if there is a relationship between two categorical variables
+
+                e.g Gender, preferred Newspapaer, Education level - Categorical var
+                is there a relationship between gender & preferred newspapaer - Chi**2 Test
+                is there a relationship between gender & Education level - Chi**2 Test
+
+                Note /Assumption:
+                    Expected freq > 5
+                    Chi**2 Test uses only the categories but not rankings.  
+                    e.g Highest education level has inherient ranking. use spearman corelation if u want to account for rankings
+
+                H0: There is no relationship
+                H1: There is a relationship
+                
+                 
+                Calculation
+                    1) contingency table
+                    2) contingency of the expected Freq ( values if the 2 variable are perfectly independent )
+                    3) Chi**2 = sum ( (Ok - Ek)**2 / Ek  )
+                    4) Degree of Freedom = (no of rows -1 ) (no of columns -1 )
+                    5) if Chi**2 value is greater than Critical value then reject the null hypothesis
+
+
+
+
+
+
+
+
+
+
+
             
+
+
+                
+
+
+
+
+
+
+
+
+
+
+                
+
 
 
 
